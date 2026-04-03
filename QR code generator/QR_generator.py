c91059a64d0d01e4941f9565bba71da9d95e204e
Seemit19 <ceemitthkmc@gmail.com>
2026-04-03 20:47:48 +0530

    This ia the first day of the week, and I am excited to share with you a simple QR code generator using Python.

---
+++ QR code generator/QR_generator.py
@@ -0,0 +1,4 @@
+import qrcode as qr
+
+img = qr.make('Hello guys, this is a QR code generator using Python. You can create your own QR code by changing the text in the make() function. Enjoy!')
+img.save("qr_code.png")
